44fde03ef3ac9e763b6246ea1d581dd74b360b55
andregustavoxs <andrewinstitucional@gmail.com>
2024-09-21 16:45:37 -0300

    Voltando para a funcionalidade de criar usuário

---
+++ src/main/java/tech/buildrun/agregadorinvestimentos/controller/UserController.java
@@ -0,0 +1,30 @@
+package tech.buildrun.agregadorinvestimentos.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.buildrun.agregadorinvestimentos.controller.service.UserService;
+import tech.buildrun.agregadorinvestimentos.entity.User;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("v1/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto) {
+        var userId = userService.createUser(createUserDto);
+        return ResponseEntity.created(URI.create("/v1/users" + userId.toString())).build();
+    }
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userid") String userId) {
+        return null;
+    }
+
+}
---
+++ src/main/java/tech/buildrun/agregadorinvestimentos/controller/service/UserService.java
@@ -0,0 +1,32 @@
+package tech.buildrun.agregadorinvestimentos.controller.service;
+
+import org.springframework.stereotype.Service;
+import tech.buildrun.agregadorinvestimentos.controller.CreateUserDto;
+import tech.buildrun.agregadorinvestimentos.entity.User;
+import tech.buildrun.agregadorinvestimentos.repository.UserRepository;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UUID createUser(CreateUserDto createUserDto) {
+        var entity = new User(
+                UUID.randomUUID(),
+                createUserDto.username(),
+                createUserDto.email(),
+                createUserDto.password(),
+                Instant.now(),
+                null);
+
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserId();
+    }
+}
