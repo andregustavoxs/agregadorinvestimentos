a4262afa8d3d38d76540086863ef38274dfc7e39
andregustavoxs <andrewinstitucional@gmail.com>
2024-10-03 11:03:05 -0300

    Criar o endpoint POST /stocks

---
+++ src/main/java/tech/buildrun/agregadorinvestimentos/controller/StockController.java
@@ -0,0 +1,24 @@
+package tech.buildrun.agregadorinvestimentos.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.buildrun.agregadorinvestimentos.controller.dto.CreateAccountDto;
+import tech.buildrun.agregadorinvestimentos.controller.dto.CreateStockDto;
+import tech.buildrun.agregadorinvestimentos.controller.dto.CreateUserDto;
+import tech.buildrun.agregadorinvestimentos.service.StockService;
+
+@RestController
+@RequestMapping("/v1/stocks")
+public class StockController {
+    private StockService stockService;
+
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> createStock(@RequestBody CreateStockDto createStockDto) {
+        stockService.createStock(createStockDto);
+        return ResponseEntity.ok().build();
+    }
+}
